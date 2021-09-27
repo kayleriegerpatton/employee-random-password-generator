6cfb65da72838997634c07e5e98dfed2738ec1c6
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-09-27 23:30:57 +0100

    pseudocode get password criteria

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,12 +3,43 @@ var generateBtn = document.querySelector("#generate");
 
 // Your code goes here
 function generatePassword() {
-  //get password criteria
   const getCriteria = function () {
     return;
   };
   //
 }
+
+// get password length
+  const passwordLength = getPasswordLength ();
+  // check passwordLength is >=8 and =<128
+// get password character choices
+  const isLowercase = getIsLowercase();
+  const isUppercase = getIsUppercase();
+  const isNumeric = getIsNumeric();
+  const isSpecial = getIsSpecial();
+// check if choices are invalid (all false)
+// if yes - alert and break
+// if no - return password criteria
+
+// create password criteria object
+const passwordCriteria = {
+  passwordLength = getPasswordLength (),
+  isLowercase = getIsLowercase (),
+  isUppercase = getIsUppercase (),
+  isNumeric = getIsNumeric (),
+  isSpecial = getIsSpecial (),
+};
+
+// generate random password
+  // for loop through the value of passwordLength
+  for (let i = 0; i<passwordLength + 1; i++)
+    // getRandomIndexNumber between 1 and passwordLength
+    const getRandomIndexNumber = Math.floor(Math.random() * passwordLength-1)
+    // pick random object array using randomIndexNumber
+    // pick random character from object array
+    // push character into passwordCharacters array (how/where to declare this?)
+// writePassword to page
+
 
 // GLOBAL VARIABLES
 const lowerCase = [
