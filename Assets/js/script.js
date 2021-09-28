6a5b504ae5306cb12419031955ed4e45e322c306
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-09-28 18:20:58 +0100

    rearranged code and sorted functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// FUNCTION DECLARATIONS (how to do steps)
-function generatePassword() {
-  // All code goes here
-  const getCriteria = function () {
-    return;
-  };
-}
-
-const getPasswordLength = function () {
-  const passwordLength = prompt("Please select a password length between 8 and 128 characters.");
-  return passwordLength;
-}
-// check password length is a number (not a string)
-const validateIsNumber = function (passwordLength) {
-  const notValidNumber = (isNaN(passwordLength))
-  return notValidNumber;
-}
-// check password length is >=8 and <= 128
-const validateNumberValue = function (passwordLength) {
-  const validValue = (passwordLength >= 8 || passwordLength <= 128);
-  return validValue;
-}
-
 // GLOBAL VARIABLES
 const lowerCase = [
   "a",
@@ -54,6 +27,7 @@ const lowerCase = [
   "y",
   "z",
 ];
+
 const upperCase = [
   "A",
   "B",
@@ -82,7 +56,9 @@ const upperCase = [
   "Y",
   "Z",
 ];
+
 const numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 // Special characters: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 const specialCharacters = [
   "!",
@@ -119,49 +95,80 @@ const specialCharacters = [
   "~",
 ];
 
-// APPLICATION STEPS (what to do/call from above)
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// get password length
-  const passwordLength = getPasswordLength ();
-
-  // validate it's a number response
-  const notValidNumber = validateIsNumber(passwordLength);
-
-  if (!(notValidNumber) && (validValue)) {
-
-
-  } else {
-    alert("Please enter a whole number from 8 to 128.")
-  }
-
-  // check passwordLength is >=8 and <=128
-// get password character choices
-  const isLowercase = getIsLowercase();
-  const isUppercase = getIsUppercase();
-  const isNumeric = getIsNumeric();
-  const isSpecial = getIsSpecial();
-// check if choices are invalid (all false)
-// if yes - alert and break
-// if no - return password criteria
-
-// create password criteria object
-const passwordCriteria = {
-  passwordLength = getPasswordLength(),
-  isLowercase = getIsLowercase(),
-  isUppercase = getIsUppercase(),
-  isNumeric = getIsNumeric(),
-  isSpecial = getIsSpecial(),
+const getPasswordLength = function () {
+  const passwordLength = prompt(
+    "Please select a password length between 8 and 128 characters."
+  );
+  return passwordLength;
 };
 
+// check password length is a number (not a string)
+const validateIsNumber = function (passwordLength) {
+  const notValidNumber = isNaN(passwordLength);
+  return notValidNumber;
+};
+
+// check password length is >=8 and <= 128
+const validateNumberValue = function (passwordLength) {
+  const validValue = passwordLength >= 8 || passwordLength <= 128;
+  return validValue;
+};
+
+const getCriteria = function () {
+  // get password length
+  const passwordLength = getPasswordLength();
+  const isValid =
+    validateIsNumber(passwordLength) && validateNumberValue(passwordLength);
+  if (isValid) {
+    // get password character choices
+    const isLowercase = getIsLowercase();
+    const isUppercase = getIsUppercase();
+    const isNumeric = getIsNumeric();
+    const isSpecial = getIsSpecial();
+    // check not all false
+    if (true) {
+      // remove true above
+      // create password criteria object
+      const passwordCriteria = {
+        passwordLength: passwordLength,
+        isLowercase: isLowercase,
+        isUppercase: isUppercase,
+        isNumeric: isNumeric,
+        isSpecial: isSpecial,
+      };
+      return passwordCriteria;
+    } else {
+      alert("Please select at least one character type.");
+      return;
+    }
+  } else {
+    alert("Please enter a whole number from 8 to 128.");
+    return;
+  }
+};
+
+// FUNCTION DECLARATIONS (how to do steps)
+function generatePassword() {
+  // All code goes here
+  const passwordCriteria = getCriteria();
+  console.log(passwordCriteria);
+}
+
+// APPLICATION STEPS (what to do/call from above)
+
 // generate random password
-  // for loop through the value of passwordLength
-  for (let i = 0; i<passwordLength + 1; i++)
-    // getRandomIndexNumber between 1 and passwordLength
-    const getRandomIndexNumber = Math.floor(Math.random() * passwordLength-1)
-    // pick random object array using randomIndexNumber
-    // get randomCharacterNumber between 0 and array length-1
-    // pick random character number from object array using randomCharacterNumber
-    // push character into passwordCharacters array (how/where to declare this?)
+// for loop through the value of passwordLength
+for (let i = 0; i < passwordLength + 1; i++) {
+  // getRandomIndexNumber between 1 and passwordLength
+  const getRandomIndexNumber = Math.floor(Math.random() * passwordLength - 1);
+  // pick random object array using randomIndexNumber
+  // get randomCharacterNumber between 0 and array length-1
+  // pick random character number from object array using randomCharacterNumber
+  // push character into passwordCharacters array (how/where to declare this?)
+}
 // writePassword to page
 
 // PROVIDED CODE
@@ -175,4 +182,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
